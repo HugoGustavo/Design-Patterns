843482da40f5ea8d9a002f61f1c99e555499727b
HugoGustavo <hugog.valin@gmail.com>
2018-09-17 08:00:29 -0300

    Composite

---
+++ src/composite/Employee.java
@@ -0,0 +1,35 @@
+package composite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Employee {
+	private String name;
+	private String department;
+	private int salary;
+	private List<Employee> subordinates;
+	
+	public Employee(String name, String department, int salary) {
+		this.name = name;
+		this.department = department;
+		this.salary = salary;
+		this.subordinates = new ArrayList<Employee>();
+	}
+	
+	public void add(Employee employee) {
+		subordinates.add(employee);
+	}
+	
+	public void remove(Employee employee) {
+		subordinates.remove(employee);
+	}
+	
+	public List<Employee> getSubordinates(){
+		return subordinates;
+	}
+	
+	@Override
+	public String toString() {
+		return ("Employee :[ Name : " + name + ", department : " + department + ", salary :" + salary+" ]");
+	}
+}
