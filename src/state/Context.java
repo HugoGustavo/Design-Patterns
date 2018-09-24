ac3ac49069b05a337926920c63217b1f733c4bbe
HugoGustavo <hugog.valin@gmail.com>
2018-09-24 07:39:52 -0300

    State

---
+++ src/state/Context.java
@@ -0,0 +1,21 @@
+package state;
+
+public class Context {
+	private State state;
+	
+	public Context() {
+		this.state = null;
+	}
+
+	public State getState() {
+		return state;
+	}
+
+	public void setState(State state) {
+		this.state = state;
+	}
+	
+	public void doAction() {
+		this.state.doAction(this);
+	}
+}
---
+++ src/state/StatePatternDemo.java
@@ -0,0 +1,18 @@
+package state;
+
+public class StatePatternDemo {
+
+	public static void main(String[] args) {
+		Context context = new Context();
+		
+		State startState = new StartState();
+		context.setState(startState);
+		context.doAction();
+		
+		
+		State stopState = new StopState();
+		context.setState(stopState);
+		context.doAction();
+	}
+
+}
