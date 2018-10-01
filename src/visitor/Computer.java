109c85fe3b7cd6c79076c3cdb71dffb39261b3b2
HugoGustavo <hugog.valin@gmail.com>
2018-10-01 12:32:22 -0300

    Visitor

---
+++ src/visitor/Computer.java
@@ -0,0 +1,21 @@
+package visitor;
+
+public class Computer implements ComputerPart {
+	private ComputerPart[] parts;
+	
+	public Computer() {
+		this.parts = new ComputerPart[] {
+				new Mouse(),
+				new Keyboard(),
+				new Monitor()
+		};
+	}
+	@Override
+	public void accept(ComputerPartVisitor computerPartVisitor) {
+		for ( ComputerPart part : parts ) {
+			part.accept(computerPartVisitor);
+		}
+		computerPartVisitor.visit(this);
+	}
+
+}
