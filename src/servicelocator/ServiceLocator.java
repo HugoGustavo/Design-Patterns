a7b0bca976981af8a00922acd1f472d6d598cd52
HugoGustavo <hugog.valin@gmail.com>
2018-10-03 10:54:58 -0300

    Service Locator

---
+++ src/servicelocator/ServiceLocator.java
@@ -0,0 +1,21 @@
+package servicelocator;
+
+public class ServiceLocator {
+	private static Cache cache;
+	
+	static {
+		cache = new Cache();
+	}
+	
+	public static Service getService(String jndiName) {
+		Service service = cache.getService(jndiName);
+		
+		if ( service != null ) {
+			return service;
+		}
+		InitialContext context = new InitialContext();
+		service = context.lookup(jndiName);
+		cache.addService(service);
+		return service;
+	}
+}
